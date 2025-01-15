3965afd34516930fe5babe24055cb92dda01b810
abhigyan-709 <abhigkumar709@gmail.com>
2025-01-15 20:51:12 +0530

    line counting in python

--- python-scripting/count-lines.py
+++ python-scripting/count-lines.py
@@ -3,5 +3,23 @@ Write a script to count the number of lines in a file.
 
 """
 
-def count_lies(filepath):
-    pass
+def count_lies(file_path):
+    try:
+        with open(file_path, 'r') as file:
+            content = file.readlines()
+            return len(content)
+    except FileNotFoundError:
+        print(f"The file specified '{file_path}' does not exists")
+        return None
+    except Exception as e:
+        print(f"Unexpected error occured '{e}'.")
+        return None
+
+def main():
+    context = str(input("Enter the file name with path and press enter: "))
+    print(count_lies(context))
+
+if __name__ == "__main__":
+    main()
+    
+    
