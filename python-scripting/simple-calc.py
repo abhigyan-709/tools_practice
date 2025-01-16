01a75577cb88a2a7f2c5b0669dd2998acf47f539
abhigyan-709 <abhigkumar709@gmail.com>
2025-01-16 20:17:49 +0530

    calc completed

--- python-scripting/simple-calc.py
+++ python-scripting/simple-calc.py
@@ -10,10 +10,22 @@ options = {
     7 : "Power"
 }
 
-def add():
-    pass
+def addition():
+    addition_number = int(input("How many numbers to add: "))
+    add_list = []
+
+    for i in range(addition_number):
+        number = int(input("Enter the number : "))
+        add_list.append(number)
+
+    print(sum(add_list))
+
 def subtract():
-    pass
+    a = int(input("Value of a: "))
+    b = int(input("Value of b: "))
+
+    print(a - b)
+
 def multiply():
     pass
 def divide():
@@ -31,13 +43,17 @@ def main():
     for key in options:
         print(f"{key} - {options[key]}")
 
+    try:
+        option = int(input("Choose the option to perform calculation: "))
+        print(f"You have chosen {option} to {options[option]}")
+    except ValueError:
+        print("Please type correct input")
 
-    option = int(input("Enter you choice: "))
-    if option in options:
-        print(f"You have chosen to {option} to {options[option]}")
+    if option == 1:
+        addition()
 
-    
-
+    elif option == 2:
+        subtract()
 
 if __name__ == "__main__":
     main()
